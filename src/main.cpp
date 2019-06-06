4db307746f90e22454e6d7345132b7bca8e3fd18
filip <filjac5882@op.pl>
2019-06-07 00:38:22 +0200

    Added fork() to draw simultaneously

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "Scene/Scene.cpp"
 #include "Scene/FreeImage.h"
 #include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
 
 Plane p;
 Model6DOF m;
@@ -83,75 +85,76 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 int main(int argc, char** argv)
 {
 
+    if(!fork())
     system("feh ../../test.jpg");
+    else {
 
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
+        GLFWwindow *window;
+        glfwSetErrorCallback(error_callback);
 
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
+        if (!glfwInit())
+            exit(EXIT_FAILURE);
 
-    int windowWidth = 640;
-    int windowHeight = 480;
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        int windowWidth = 640;
+        int windowHeight = 480;
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    window = glfwCreateWindow(windowWidth, windowHeight, "Simulation", nullptr, nullptr);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+        window = glfwCreateWindow(windowWidth, windowHeight, "Simulation", nullptr, nullptr);
+        if (!window) {
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
 
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetWindowRefreshCallback(window, display);
-    glfwSetWindowSizeCallback(window, reshape);
+        glfwSetKeyCallback(window, key_callback);
+        glfwSetWindowRefreshCallback(window, display);
+        glfwSetWindowSizeCallback(window, reshape);
 
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
-    glfwSwapInterval(1);
+        glfwMakeContextCurrent(window);
+        gladLoadGL();
+        glfwSwapInterval(1);
 
-    init();
-    reshape(window,windowWidth,windowHeight);
+        init();
+        reshape(window, windowWidth, windowHeight);
 
-    printHelp();
-    move(0,0,0,floorVertices);
+        printHelp();
+        move(0, 0, 0, floorVertices);
 
-    try {
+        try {
 
-        do {
-            glfwPollEvents();
-            display(window);
-            pc.simulate();
-            roll((m.getCurrentPosition().angles.roll - m.getPreviousPosition().angles.roll) * M_PI / 180,
-                 modelVertices);
-            pitch((m.getCurrentPosition().angles.pitch - m.getPreviousPosition().angles.pitch) * M_PI / 180,
-                  modelVertices);
-            yaw((m.getCurrentPosition().angles.yaw - m.getPreviousPosition().angles.yaw) * M_PI / 180, modelVertices);
-            move(m.getPreviousPosition().point.getX() - m.getCurrentPosition().point.getX(),
-                 m.getPreviousPosition().point.getZ() - m.getCurrentPosition().point.getZ(),
-                 m.getPreviousPosition().point.getY() - m.getCurrentPosition().point.getY(), modelVertices);
-            glfwSwapBuffers(window);
-        } while (!glfwWindowShouldClose(window));
-    }
-    catch (int e)
-    {
-        std::cout<<"You crashed";
+            do {
+                glfwPollEvents();
+                display(window);
+                pc.simulate();
+                roll((m.getCurrentPosition().angles.roll - m.getPreviousPosition().angles.roll) * M_PI / 180,
+                     modelVertices);
+                pitch((m.getCurrentPosition().angles.pitch - m.getPreviousPosition().angles.pitch) * M_PI / 180,
+                      modelVertices);
+                yaw((m.getCurrentPosition().angles.yaw - m.getPreviousPosition().angles.yaw) * M_PI / 180,
+                    modelVertices);
+                move(m.getPreviousPosition().point.getX() - m.getCurrentPosition().point.getX(),
+                     m.getPreviousPosition().point.getZ() - m.getCurrentPosition().point.getZ(),
+                     m.getPreviousPosition().point.getY() - m.getCurrentPosition().point.getY(), modelVertices);
+                glfwSwapBuffers(window);
+            } while (!glfwWindowShouldClose(window));
+        }
+        catch (int e) {
+            std::cout << "You crashed";
+            cleanup();
+            glfwDestroyWindow(window);
+            //glfwTerminate();
+            exit(EXIT_SUCCESS);
+        }
+
+
         cleanup();
         glfwDestroyWindow(window);
-        //glfwTerminate();
+
+        glfwTerminate();
         exit(EXIT_SUCCESS);
+
     }
-
-
-
-    cleanup();
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
-
 }
 
